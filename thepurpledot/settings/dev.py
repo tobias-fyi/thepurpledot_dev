7f67a7572604ea27fc55d05ffdc86422ef99d82a
tobias-fyi <tobyreaper@gmail.com>
2019-10-28 21:46:38 -0600

    Finished StreamFields for now

--- thepurpledot/settings/dev.py
+++ thepurpledot/settings/dev.py
@@ -4,12 +4,14 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6hm7o_*8je&#8y6+s+v&id&5w@4#bq-w%@_1bu)g-94$jo0bys'
+SECRET_KEY = "6hm7o_*8je&#8y6+s+v&id&5w@4#bq-w%@_1bu)g-94$jo0bys"
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ["*"]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+INSTALLED_APPS = INSTALLED_APPS + ["debug_toolbar"]
 
 
 try:
